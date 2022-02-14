fc64cb7afc015b7ecbc49652a05d6bc51b26323f
yaffet97 <yaffet97@gmail.com>
2022-02-14 11:27:55 +0100

    aa

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
 var anElement = document.getElementById("app");
 
-anElement.innerHTML= <button oneclick='buttonClick()'> Click me</button>;
+anElement.innerHTML= "Hello world";
+
+/*anElement.innerHTML = <button oneclick='buttonClick()'> Click me</button>;
 
 
-function buttonClick(){
+function buttonClick() {
     anElement.outerHTML = <strong>Yaffet</strong>;
-}
+}*/

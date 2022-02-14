167f0ec5aedb92ffa212560894ab844407a0a683
yaffet97 <yaffet97@gmail.com>
2022-02-14 12:25:58 +0100

    a

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 var anElement = document.getElementById("app");
 
 anElement.innerHTML= "Hello world";
+anElement.outerHTML="Helo world";
 
 anElement.innerHTML = <button oneclick='buttonClick()'> Click me</button>;
 

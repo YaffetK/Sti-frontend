19d246923e128343c8b629dcb88048c2790c24ce
yaffet97 <yaffet97@gmail.com>
2022-02-17 13:36:58 +0100

    Skapat en data.json fil

--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,25 @@
-var anElement = document.getElementById("app");
+var ettElement= document.getElementById("app")
+ettElement.innerHTML="Hello World"
 
-var anotherElement=document.getElementById('demo');
+var skapatElement= document.createElement("strong")
+skapatElement.innerHTML= "Hello My World"
 
-anotherElement.innerHTML= "Här kommer dagens datum stå:";
+var andraElement=document.getElementById("demo")
+andraElement.appendChild(skapatElement)
 
-function buttonClickForDate() {
-    console.log("Dagens datum")
-    anotherElement.innerHTML= Date();
+setTimeout(age,3000)
+
+function age(){
+ console.log("aging game")
+ setTimeout(age,3000)
 }
 
-var ettElement=document.createElement("strong");
-ettElement.innerHTML("Hello My World");
-
-var app=getElementById("app")
-app.appendChild(anElement);
-
-
-
-anElement.innerHTML= "Hello world";
-console.log(anElement);
-
-
-//anElement.innerHTML = "<button onClick='buttonClick()'> Click me</button>";
-
+function buttonClickForDate(){
+    console.log("Today´s date is:")
+    console.log(Date())
+}
 
 function buttonClick() {
     console.log("Klicked at button!")
-    anElement.outerHTML = "<strong>Yaffet</strong>";
+    skapatElement.outerHTML = "<strong>Yaffet</strong>"
 }
